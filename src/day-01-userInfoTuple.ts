3fea3e4859d86a1b47ee4354335dded65c45300d
Prince <anakwaprince204@gmail.com>
2026-01-01 22:25:19 +0000

    feat(day-01): working with tuples

---
+++ src/day-01-userInfoTuple.ts
@@ -0,0 +1,7 @@
+const userInfo: [number, string, boolean] = [ 123, 'Prince', true ];
+
+const userID = userInfo[0];
+const userName = userInfo[1];
+const activeStatus = userInfo[2];
+
+console.log(`${userID}, ${userName}, ${activeStatus}`)
