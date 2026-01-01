f2d5ee4a60cb66dbe08f1bcbf980450712f65458
Prince <anakwaprince204@gmail.com>
2026-01-01 21:27:14 +0000

    feat(day-01): declare strongly typed basic variables

---
+++ src/day-01-types.ts
@@ -0,0 +1,11 @@
+let username: string;
+let age: number;
+let isVerified: boolean;
+
+username = 'Prince';
+age = 25;
+isVerified = true;
+
+if(isVerified)
+    console.log(`My name is ${username} and I am ${age} years old`)
+
