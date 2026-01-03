465e5db57edef1422633753ebc947bf563da7bce
Prince <anakwaprince204@gmail.com>
2026-01-03 20:27:20 +0000

    Day-03-Redo-shapeInterface

---
+++ src/Day 3/day-03-shapeInterface.ts
@@ -0,0 +1,30 @@
+interface Shape {
+    area(): number;
+}
+
+
+class Square implements Shape {
+    side: number;
+    constructor(side: number) {
+        this.side = side
+    }
+    area(): number {
+        return this.side * this.side;
+    }
+}
+
+class Circle implements Shape {
+    radius: number;
+    constructor(radius: number) {
+        this.radius = radius;
+    }
+    area(): number {
+        return Math.PI * this.radius * this.radius
+    }
+}
+
+const square = new Square(5);
+const circle = new Circle(4);
+
+console.log(square.area());
+console.log(circle.area());
