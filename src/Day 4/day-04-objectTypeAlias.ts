cfb837a1d2e9dbb33f4bfc47f9862bd84d48b6c1
Prince <anakwaprince204@gmail.com>
2026-01-04 19:29:52 +0000

    Day-04-bonus-working with object type alias

---
+++ src/Day 4/day-04-objectTypeAlias.ts
@@ -0,0 +1,15 @@
+type Product = {
+    id: number,
+    name: string,
+    price: number,
+};
+
+const good: Product = {
+    id: 25,
+    name: "Coca-Cola",
+    price: 399,
+}
+
+console.log(good.id);
+console.log(good.name);
+console.log(good.price);
