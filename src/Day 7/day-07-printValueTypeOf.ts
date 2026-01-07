56c682c794de293bd0b11d73ed982b2a319f66e7
P-cyber162 <anakwaprince204@gmail.com>
2026-01-07 21:55:22 +0000

    Using Typeof in typescript

---
+++ src/Day 7/day-07-printValueTypeOf.ts
@@ -0,0 +1,11 @@
+const printUser = (value: string | number ): void => {
+    if(typeof value === "string") {
+        console.log(`String value: ${value}`);
+    }
+    else{
+        console.log(`Number value: ${value}`);
+    }
+}
+
+printUser(25);
+printUser('Hello');
